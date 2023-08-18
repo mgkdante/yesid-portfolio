49b8d3ebe491b0ea7620e1612f2beda5d67905ca
Yesid Otalora <mgkdante@pm.me>
2023-08-18 00:40:31 -0400

    NavBar part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-const App = ()=> {
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Hero, Works, Experience, Feedbacks, Tech, Navbar, StarsCanvas } from './components';
 
+const App = () => {
   return (
-    <div>
-      Yesid 3D portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+        <Navbar />
+        <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
