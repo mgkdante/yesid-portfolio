f712a8ff0a1b3e58049ff02546c9cd2d6a3b0ae1
Yesid Otalora <mgkdante@pm.me>
2023-08-19 01:23:20 -0400

    setting gh pages part 8

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/yesid-portfolio/',
+    base: '/',
   }
 
   if (command !== 'serve') {
