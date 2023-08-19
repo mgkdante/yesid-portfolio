3758b21a053bc671064d226fdfae2c6afddccbe3
Yesid Otalora <mgkdante@pm.me>
2023-08-18 22:08:34 -0400

    navBar part2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+
   return (
-    <nav className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to='/' className='flex items-center gap-2'
+    <nav
+      className={
+        "${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary"
+      }
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
           onClick={() => {
             setActive("");
+            window.scrollTo(0, 0);
           }}
         >
+          <img src={logo} alt="logo" className="w-100 h-100 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Yesid <span className="sm:block hidden">Otalora</span>
+          </p>
         </Link>
+
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex items-center flex-1 justify-end">
+          <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[h-28px] cursor-pointer object-contain"
+          onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' :'flex'} p-6`}>
+          </div>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
