7cb364fc4bc2d2a82b9eb8a718f0aeec8a19a774
Yesid Otalora <mgkdante@pm.me>
2023-08-19 00:56:06 -0400

    setting gh pages - needed to add -dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/yesid-portfolio/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/yesid-portfolio/'
+  }
+
+  return config
 })
