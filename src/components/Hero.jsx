136374d6153e10268f79ef2e300fd612f5c7ed42
Yesid Otalora <mgkdante@pm.me>
2023-08-20 01:33:58 -0400

    hero part3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,54 @@
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className=' w-full h-screen relative mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex fex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#e77917]' />
-          <div className='w-1 sm:h-80 h-40 orange-gradient' />
+    <section className=" w-full h-screen relative mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex fex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#e77917]" />
+          <div className="w-1 sm:h-80 h-40 orange-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className='text-[#e77917]'>Yesid</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi! I'm <span className="text-[#e77917]">Yesid</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a <span className='text-[#e77917]'>Frontend Developer</span> based in Quebec, I love to create beautiful and functional websites.
+            I'm a <span className="text-[#e77917]">Frontend Developer</span>{" "}
+            based in Quebec, I love to create beautiful and functional websites.
           </p>
         </div>
       </div>
       <ComputersCanvas />
 
+      <div
+        className="absolute xs:bottom-10 bottom-32 w-full
+        flex justify-center items-center"
+      >
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl border-4
+          border-secondary flex justify-center items-start p-2"
+          >
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                repeat: Infinity,
+                duration: 4,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-[#e77917] mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,47 +1,68 @@
-import { Suspense, useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const Computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ( isMobile ) => {
+  const Computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={3}
-      groundColor="black"/>
+      <hemisphereLight intensity={3} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
-      position={[-20, 50, 10]}
-      angle={0.12}
-      penumbra={1}
-      intensity={1}/>
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+      />
       <primitive
-      object={Computer.scene}
-      scale={1}
-      position={[0, -4.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}/>
+        object={Computer.scene}
+        scale={isMobile ? 0.75 :0.5}
+        position={isMobile ? [0, -3, -2.2]:[0, -3.5, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  return(
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQuertChange = (e) => {
+      setIsMobile(e.matches);
+
+      mediaQuery.addEventListener("change", handleMediaQuertChange);
+
+      return () => {
+        mediaQuery.removeEventListener("change", handleMediaQuertChange);
+      };
+    };
+  }, []);
+
+  return (
     <Canvas
-    frameloop="demand"
-    shadows
-    camera={{ position: [20, 3, 5], fov: 50}}
-    gl={{preserveDrawingBuffer: true}}
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 30 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls enableZoom={false}
-      maxPolarAngle={Math.PI /2}
-      minPolarAngle={Math.PI /2} />
-      <Computers />
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas
+export default ComputersCanvas;
