c848c0f3a239530b76391be2451080d5e76a6167
Yesid Otalora <mgkdante@pm.me>
2023-08-20 22:59:50 -0400

    about part1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,34 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../animation";
+import {SectionWrapper}  from "../hoc/index";
 
 const ServiceCard = ({ index, title, icon }) => {
-  return <p>{title}</p>;
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradent p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[16px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
 };
 
 const About = () => {
   return (
-    <div className="flex sm:flex-row flex-col justify-center items-center gap-8 sm:ps-[10svw] sm:pe-[10svw] ps-[5svw] pe-[5svw]">
+    <div>
       <motion.div>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
@@ -31,11 +51,11 @@ const About = () => {
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} />
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </div>
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
