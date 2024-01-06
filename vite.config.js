13b873b93fe3caec8e17be74d27694bacdef92ec
yesid <mgkdante@pm.me>
2024-01-06 04:11:23 -0500

    Fxing package.json so I can keep custom URL on deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = {
+  return {
     plugins: [react()],
     base: '/',
   }
-
- /* if (command !== 'serve') {
-    config.base = '/yesid-portfolio/'
-  }*/
-
-  return config
 })
