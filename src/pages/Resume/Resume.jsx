c3704610218e5adbf26241bebba5589f7d495324
Yesid <mgkdante@pm.me>
2024-09-29 19:05:06 -0400

    Starting overhaul of resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { motion } from "framer-motion";
-import ResumeLeftColumn from "./ResumeLeftColumn";
-import ResumeRightColumn from "./ResumeRightColumn";
-import DownloadButton from "./DownloadButton";
+import React from "react"
+import { motion } from "framer-motion"
+import ResumeLeftColumn from "./ResumeLeftColumn"
+import ResumeRightColumn from "./ResumeRightColumn"
+import DownloadButton from "./DownloadButton"
 const Resume = () => {
   return (
     <>
-      <motion.div
+      {/*} <motion.div
         layout
         className="relative grid md:grid-cols-6 grid-rows-1 text-white mt-5"
       >
         <ResumeLeftColumn />
         <ResumeRightColumn />
-      </motion.div>
+      </motion.div> {*/}
       <DownloadButton />
     </>
-  );
-};
+  )
+}
 
-export default Resume;
+export default Resume
